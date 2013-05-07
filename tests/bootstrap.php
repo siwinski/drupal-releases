a473cac51c2fe0e0c11fba39d723b0a98f332c20
Shawn Iwinski <siwinski@redhat.com>
2013-05-07 15:33:19 -0400

    Added initial tests setup

---
+++ tests/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * This file is part of the Drupal Updates package.
+ *
+ * (c) Shawn Iwinski <shawn.iwinski@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+if (!is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
+    echo 'Could not find "vendor/autoload.php". Did you forget to run "composer install --dev"?'.PHP_EOL;
+    exit(1);
+}
+
+require_once $autoloadFile;
