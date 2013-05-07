50c6d3058fcdc919a35ffa4966f0d711394fc8c6
Shawn Iwinski <siwinski@redhat.com>
2013-05-07 15:30:42 -0400

    Added initial Terms

---
+++ src/SAI/DrupalReleases/Terms.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of the Drupal Updates package.
+ *
+ * (c) Shawn Iwinski <shawn.iwinski@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SAI\DrupalReleases;
+
+/**
+ * @author Shawn Iwinski <shawn.iwinski@gmail.com>
+ */
+class Terms extends \ArrayObject
+{
+    /**
+     * @todo Document
+     */
+    public function __construct(\SimpleXMLElement $terms)
+    {
+        $array = array();
+
+        foreach ($terms as $term) {
+            $termName = (string) $term->name;
+
+            if (!isset($array[$termName])) {
+                $array[$termName] = array();
+            }
+
+            $array[$termName][] = (string) $term->value;
+        }
+
+        parent::__construct($array, \ArrayObject::STD_PROP_LIST);
+    }
+}
