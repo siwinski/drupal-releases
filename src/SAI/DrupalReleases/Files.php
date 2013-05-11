27d04f8df92f48b8d2b1c0e696e09996f2c3d900
Shawn Iwinski <shawn.iwinski@gmail.com>
2013-05-11 19:16:41 -0400

    [Files] Initial implementation

---
+++ src/SAI/DrupalReleases/Files.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file is part of the Drupal Releases package.
+ *
+ * (c) Shawn Iwinski <shawn.iwinski@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SAI\DrupalReleases;
+
+/**
+ * <files>
+ *     <file>...</file>
+ *     <file>...</file>
+ *     <file>...</file>
+ *     ...
+ * </files>
+ *
+ * @author Shawn Iwinski <shawn.iwinski@gmail.com>
+ */
+class Files extends \ArrayObject
+{
+
+    /**
+     *
+     */
+    public function __construct(\SimpleXMLElement $files)
+    {
+        $array = array();
+
+        foreach ($files as $file) {
+            $key = (string) $file->archive_type;
+            $array[$key] = new File($file);
+        }
+
+        parent::__construct($array, \ArrayObject::STD_PROP_LIST);
+    }
+
+    /**
+     *
+     */
+    public function download()
+    {
+        // TODO
+    }
+
+}
