d5cc04d37b9e795cf3527de852087688f5247231
Shawn Iwinski <siwinski@redhat.com>
2013-05-16 17:32:37 -0400

    [Tests/*] Added some additional test placeholders

---
+++ tests/SAI/DrupalReleases/Tests/Console/Command/SearchCommandTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is part of the Drupal Releases package.
+ *
+ * (c) Shawn Iwinski <shawn.iwinski@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SAI\DrupalReleases\Tests\Console\Command;
+
+use SAI\DrupalReleases\Console\Command\SearchCommand;
+
+/**
+ * @author Shawn Iwinski <shawn.iwinski@gmail.com>
+ */
+class SearchCommandTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Placeholder/dummy test
+     */
+    public function testPlaceholder()
+    {
+        $this->assertTrue(true);
+    }
+
+}
